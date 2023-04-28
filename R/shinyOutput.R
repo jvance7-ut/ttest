e9f191a9f634384ef882d689b84149c69a4d22b8
Jennifer Vance <jennywinger03@gmail.com>
2023-04-28 13:04:29 -0600

    shiny integration

---
+++ R/shinyOutput.R
@@ -0,0 +1,17 @@
+#' @title Dynamic output using shiny
+#'
+#'
+#'@desccription Widgets, plot and run ttest on x and y
+#'
+#' @return Plots dataframe CI
+#'
+#' @section shiny: The shiny web server will be used.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{shinyOutput(funObject$x, funObject$y, ...)}
+shinyOutput<-function(){
+  shiny::runApp(system.file("shiny1/example", package = "ttest"),
+                launch.browser = TRUE)
+}
